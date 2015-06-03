d8fd2f851b880d77721c3a4fd8603fca40680807
CrRinko <crimson_rin@yeah.net>
2015-06-03 21:43:35 +0800

    Create subsys

---
+++ subsys.cpp
@@ -0,0 +1,10 @@
+//
+//  subsys.cpp
+//  MemoryManagement
+//
+//  Created by 刘 照阁 on 15/6/3.
+//  Copyright (c) 2015年 Crmson's Code Project. All rights reserved.
+//
+
+#include "memsys.h"
+using namespace subsys;
